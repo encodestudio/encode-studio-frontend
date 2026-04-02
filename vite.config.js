1a1cb2ff7ab9c73b9181593c8833176617b9e1f1
Shivam <encodestudio.in@gmail.com>
2026-04-02 19:03:28 +0530

    fix: correct vercel rewrites

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      "encodestudio.in",
+      "encode-studio-backend.vercel.app",
+      "encode-studio-frontend.vercel.app"
+    ]
+  }
 })
